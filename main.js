3891cec16bb4c26df986b724bcbc69d05293bf91
puri <puripant@gmail.com>
2017-07-20 17:04:51 +0700

    Remove anti-alias artifacts from the images Get image values from all color channels

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
-d3.request("data/Thailand_provinces_mk-from-gif.tiff").responseType("arraybuffer").get(function(error, request) {
+var svg = d3.select("svg");
+var width = +svg.attr("width");
+var height = +svg.attr("height");
+
+d3.xml("data/Thailand_provinces_mk.svg").mimeType("image/svg+xml").get(function(error, xml) {
+  if (error) throw error;
+  document.body.appendChild(xml.documentElement);
+  d3.selectAll("svg")
+    .attr("width", width)
+    .attr("height", height);
+});
+
+d3.request("data/Thailand_provinces_mk-color.tiff").responseType("arraybuffer").get(function(error, request) {
   if (error) throw error;
 
-  var tiff = GeoTIFF.parse(request.response),
-      image = tiff.getImage(),
-      values = image.readRasters()[0],
-      m = image.getHeight(),
-      n = image.getWidth(),
-      svg = d3.select("svg"),
-      width = +svg.attr("width"),
-      height = +svg.attr("height");
+  var tiff = GeoTIFF.parse(request.response);
+  var image = tiff.getImage();
+  var rgba = image.readRasters();
+  var threshold = function(value) {
+    return (value < 128)? 0:1;
+  }
+  var values = rgba[0].map(function(r, i) {
+    return threshold(r)*4 + threshold(rgba[1][i])*2 + threshold(rgba[2][i])
+  });
+  var m = image.getHeight();
+  var n = image.getWidth();
 
   // downsampling values
-  var downsamplingRatio = 20, // must be int
-      downsamplingWidth = Math.ceil(n / downsamplingRatio);
-      downsamplingHeight = Math.ceil(m / downsamplingRatio);
+  var downsamplingRatio = 15; // must be int
+  var downsamplingWidth = Math.ceil(n / downsamplingRatio);
+  var downsamplingHeight = Math.ceil(m / downsamplingRatio);
   var values2 = [];
   for (var j = 0; j < m; j += downsamplingRatio) {
     for (var i = 0; i < n; i += downsamplingRatio) {
@@ -33,5 +48,5 @@ d3.request("data/Thailand_provinces_mk-from-gif.tiff").responseType("arraybuffer
     .data(contours(values2))
     .enter().append("path")
       .attr("d", d3.geoPath(d3.geoIdentity().scale(width / downsamplingWidth)))
-      .attr("fill", function(d) { return color(d.value); });
+      .attr("fill", function(d) { return color(Math.round(d.value)); });
 });
